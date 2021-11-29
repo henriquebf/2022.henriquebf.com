a9e47a13294877d2a208a2ec29b3d83f77046da0
Henrique Ferreira <henriquebf@gmail.com>
2021-11-29 13:09:03 +0100

    update index and app

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,16 +2,29 @@ import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div id="app">
       <Component {...pageProps} />
       <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css?family=Share+Tech+Mono');
+
         html,
         body {
+          width: 100%;
+          height: 100%;
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          -webkit-font-smoothing: antialiased;
+          letter-spacing: 0.001em;
+        }
+
+        html {
+          background-color: #fff;
+          box-sizing: border-box;
+          color: #222;
+          font-family: 'Share Tech Mono', Helvetica;
+          font-size: 16px;
+          font-weight: normal;
+          line-height: 1.5;
         }
 
         a {
@@ -22,8 +35,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         * {
           box-sizing: border-box;
         }
+
+        #app {
+          height: 100%;
+        }
       `}</style>
-    </>
+    </div>
   );
 }
 
