a11ac46a81a897e0ad1f9d8058e6c07818398e9a
Henrique Ferreira <henriquebf@gmail.com>
2021-12-07 13:28:53 +0100

    fix svg

--- web/components/svg/IconExternalLink.tsx
+++ web/components/svg/IconExternalLink.tsx
@@ -19,8 +19,8 @@ const Svg: NextPage<Props> = ({ size }) => {
         fill="none"
         stroke="currentColor"
         strokeWidth="2"
-        stroke-Linecap="round"
-        stroke-Linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
         <polyline points="15 3 21 3 21 9"></polyline>
