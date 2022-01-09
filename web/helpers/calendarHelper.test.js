9edd0971e1bf32b05bf02f9e37632e50edd4e129
Henrique Ferreira <henriquebf@gmail.com>
2022-01-09 20:26:15 +0100

    add tests for helpers

---
+++ web/helpers/calendarHelper.test.js
@@ -0,0 +1,49 @@
+import { getMonth, getHours } from './calendarHelper';
+
+describe('getHours', () => {
+  const testDate = new Date(2022, 0, 9);
+
+  it('Current test month should be January', () => {
+    const { name } = getMonth(0, testDate);
+    expect(name).toEqual('Jan');
+  });
+
+  it('Next test month should be February', () => {
+    const { name } = getMonth(1, testDate);
+    expect(name).toEqual('Feb');
+  });
+
+  it('Current test year should be 2022', () => {
+    const { year } = getMonth(0, testDate);
+    expect(year).toEqual(2022);
+  });
+
+  it('20 months ahead should be 2023', () => {
+    const { year } = getMonth(20, testDate);
+    expect(year).toEqual(2023);
+  });
+});
+
+describe('getHours', () => {
+  const settings = '2022:Jan:0,2022:Feb:0,2022:Mar:0,2022:Apr:32,2022:May:72';
+
+  it('January should be unavailable', () => {
+    const hours = getHours('Jan', 2022, settings);
+    expect(hours).toEqual(0);
+  });
+
+  it('April should have 32 hours', () => {
+    const hours = getHours('Apr', 2022, settings);
+    expect(hours).toEqual(32);
+  });
+
+  it('A not defined month should return 128', () => {
+    const hours = getHours('Jul', 2023, settings);
+    expect(hours).toEqual(128);
+  });
+
+  it('Settings not defined month should return 128', () => {
+    const hours = getHours('Mar', 2022);
+    expect(hours).toEqual(128);
+  });
+});
