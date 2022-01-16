4c9686bacd0c32d0570741fe4594e1b7644bd572
Henrique Ferreira <henriquebf@gmail.com>
2022-01-16 21:29:33 +0100

    chore: add twitter metadata

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -23,9 +23,13 @@ const Home: NextPage<Props> = ({ goal, availabilities }) => {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content="/meta-opengraph.jpg" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content="/meta-opengraph.jpg" />
+        <meta property="og:type" content="website" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <Header />
